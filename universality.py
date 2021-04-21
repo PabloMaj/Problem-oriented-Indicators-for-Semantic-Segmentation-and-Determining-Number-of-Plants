332af69c084e9db1c558b11728aea856af40d840
PabloMaj <39468585+PabloMaj@users.noreply.github.com>
2021-04-21 18:27:15 +0200

    Update source code

--- universality.py
+++ universality.py
@@ -1,8 +1,11 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.metrics import f1_score
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from utilities import calculate_optimal_threshold, calculate_VI_RGB, calculate_VI_fraction
+from skimage.filters import threshold_otsu
 
 def read_data(dates=None, path_to_data=None):
 
@@ -22,6 +25,7 @@ def read_data(dates=None, path_to_data=None):
 
 dates = ["2019_07_25", "2019_09_20", "2019_10_11"]
 path_to_data = "data//Segmentation//"
+
 results_file = open("Results_robustness.txt", "w")
 results_file.write("Model_type\tTrain_date\tTest_date\tF1_score_test\n")
 results_file.close()
@@ -54,7 +58,12 @@ for VI_name in ["ExG", "VDVI"]:
     for train_date in dates:
         for test_date in dates:
             Y_predict_test = VI_RGB_test[test_date][VI_name]
-            Y_predict_test = (Y_predict_test > threshold_VI[train_date][VI_name])
+
+            thresh = threshold_otsu(image=Y_predict_test, nbins=1000)
+            Y_predict_test = (Y_predict_test > thresh)
+
+            # Y_predict_test = (Y_predict_test > threshold_VI[train_date][VI_name])
+
             F1_score = f1_score(Y_predict_test, Y_test[test_date])
             print(f"Train_date:{train_date}")
             print(f"Test_date:{test_date}")
@@ -76,7 +85,12 @@ for train_date in dates:
 for train_date in dates:
     for test_date in dates:
         Y_predict_test = models[train_date].predict(X_test[test_date])
-        Y_predict_test = (Y_predict_test > threshold[train_date])
+
+        thresh = threshold_otsu(image=Y_predict_test, nbins=1000)
+        Y_predict_test = (Y_predict_test > thresh)
+
+        # Y_predict_test = (Y_predict_test > threshold[train_date])
+
         F1_score = f1_score(Y_predict_test, Y_test[test_date])
         print(f"Train_date:{train_date}")
         print(f"Test_date:{test_date}")
@@ -88,10 +102,9 @@ for train_date in dates:
 print("3. Optimized Fraction Indicators")
 
 VI_fraction_coeffs = dict()
-VI_fraction_coeffs["2019_07_25"] = [36.80527083, -64.29557348, 30.29631855, -20.51248141, -17.49035232, -88.2301978]
-VI_fraction_coeffs["2019_08_19"] = [5.60702874, -7.99014732, 5.33548688, -0.77724135, -3.57582202, -0.10127517]
-VI_fraction_coeffs["2019_09_20"] = [26.71492253, -34.8274881, 23.80132208, 11.11393239, -20.81248397, 8.17381451]
-VI_fraction_coeffs["2019_10_11"] = [0.08974777, -3.95925329, 1.19164462, 0.52334907, -1.0482393, 1.24637182]
+VI_fraction_coeffs["2019_07_25"] = [-2.17140641, 3.70160468, -2.5102769, 2.88246843, 0.82232753, -1.68856676]
+VI_fraction_coeffs["2019_09_20"] = [0.16585135, -0.78082026, 0.6213656, -1.07198113, -1.14975748, 0.7004156]
+VI_fraction_coeffs["2019_10_11"] = [0.21016317, -2.23698845, 2.51774888, -2.93230797, -0.02953425, -2.48650525]
 for date in dates:
     VI_fraction_coeffs[date] = np.array(VI_fraction_coeffs[date])
 
@@ -99,10 +112,50 @@ for train_date in dates:
     for test_date in dates:
         Y_predict_test = calculate_VI_fraction(
             X=X_test[test_date], w_up=VI_fraction_coeffs[train_date][:3], w_down=VI_fraction_coeffs[train_date][3:])
-        F1_score = f1_score(Y_predict_test > 0.2, Y_test[test_date])
+
+        thresh = threshold_otsu(image=Y_predict_test, nbins=1000)
+        Y_predict_test = (Y_predict_test > thresh)
+
+        F1_score = f1_score(Y_predict_test, Y_test[test_date])
         print(f"Train_date:{train_date}")
         print(f"Test_date:{test_date}")
         print(f"F1_score:{np.round(F1_score, 4)}")
         results_file = open("Results_robustness.txt", "a")
         results_file.write(f"fraction_optimized\t{train_date}\t{test_date}\t{np.round(F1_score, 4)}\n")
         results_file.close()
+
+# visualization
+dataset_names = {"2019_07_25": "flowering", "2019_09_20": "mature", "2019_10_11": "before harvest"}
+
+fig, axs = plt.subplots(3, 3, figsize=(14, 14))
+MaskRCNN_scores = [0.6613, 0.7013, 0.6316, 0.7758, 0.6341, 0.8412]
+model_types = ["ExG", "VDVI", "Optimised\nlinear", "Optimised\nfraction", "Mask\nR-CNN"]
+model_types = [model_types[i] for i in [0, 2, 1, 3, 4]]
+df_results = pd.read_csv("Results_robustness.txt", sep="\t")
+counter = 0
+for train_counter, train_date in enumerate(dates):
+    for test_counter, test_date in enumerate(dates):
+        pos_1 = test_counter
+        pos_2 = train_counter
+        if train_date != test_date:
+            df_filtered = df_results[(df_results["Train_date"] == train_date) & (df_results["Test_date"] == test_date)]
+            y = list(df_filtered["F1_score_test"]) + [MaskRCNN_scores[counter]]
+            axs[pos_1, pos_2].grid()
+            y = [y[i] for i in [0, 2, 1, 3, 4]]
+            print(y)
+            axs[pos_1, pos_2].bar(model_types, y, color=["tab:blue", "tab:green", "orange", "plum", "tomato"], zorder=3, edgecolor="k")
+            axs[pos_1, pos_2].set_ylim(0.6, 0.87)
+            axs[pos_1, pos_2].set_ylabel("F1-score")
+            axs[pos_1, pos_2].set_title(f"Train: {dataset_names[train_date]}\nTest: {dataset_names[test_date]}")
+            counter += 1
+        else:
+            axs[pos_1, pos_2].set_xticks([])
+            axs[pos_1, pos_2].set_yticks([])
+            axs[pos_1, pos_2].text(0.5, 0.5, 'Figure 7',
+            bbox={'facecolor': 'white', 'alpha': 1, 'edgecolor': 'none', 'pad': 1},
+            ha='center', va='center', fontsize=20)
+            axs[pos_1, pos_2].set_title(f"Train: {dataset_names[train_date]}\nTest: {dataset_names[test_date]}")
+
+
+plt.tight_layout()
+plt.savefig("Universality_results.png", dpi=300)
